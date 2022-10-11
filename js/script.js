a3673172bf1fc5410b90b00637c04049d2255ec5
Mattia-Giorgetti <mattiagiorgetti37@gmail.com>
2022-10-11 18:28:29 +0200

    minor fix + aggiunto style

--- js/script.js
+++ js/script.js
@@ -11,17 +11,15 @@ const accedi = function(){
         let mailElement = mailList[i];
 
         if (mailElement === userMail){
-            match == true;
+            match = true;
         }
     }
     if(match == true){
-        document.querySelector('.welcome_user').innerHTML = `Puoi accedere`;
+        document.querySelector('.welcome_user').innerHTML = `Benvenuto! Inizia a giocare!`;
+        document.querySelector('#my_roll').classList.add('show');
     } else {
-        document.querySelector('.welcome_user').innerHTML = `Alt`;
+        document.querySelector('.welcome_user').innerHTML = `Alt! Non puoi entrare`;
     }
-    
-
-
 }
 mailSubmitButton.addEventListener('click', accedi);
 
@@ -33,9 +31,9 @@ const tiroRandom = function(){
     const numeriDaGenerare = [6];
     for(let i = 1; i <= numeriDaGenerare; i++){
        let numeriGeneratiUser = Math.floor((Math.random() * 6) + 1);
-       document.querySelector('.user_result').innerHTML = `Il tuo tiro è: ${numeriGeneratiUser}`;
+       document.querySelector('.user_result').innerHTML = `Il tuo tiro è: <span class="text-primary">${numeriGeneratiUser}</span>`;
        let numeriGeneratiComputer = Math.floor((Math.random() * 6) + 1);
-       document.querySelector('.computer_result').innerHTML = `Il tiro del Computer è: ${numeriGeneratiComputer}`;
+       document.querySelector('.computer_result').innerHTML = `Il tiro del Computer è: <span class="text-danger">${numeriGeneratiComputer}</span>`;
        if(numeriGeneratiUser > numeriGeneratiComputer){
         document.querySelector('.final_result').innerHTML = `Complimenti Hai Vinto! .... Ritenta? `;
     } else if (numeriGeneratiUser < numeriGeneratiComputer){
